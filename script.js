bdea1efb7cdeafb924b7d2424565daaa4cadcb93
WO| coltexe <brawl.colt.dina@gmail.com>
2025-11-06 13:52:48 +0000

    feat: Maîtrise des variables et conditions

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 console.log("Laboratoire prêt !")
+
+const nom = "POMMEREL";
+console.log(nom)
+
+let age = 17;
+console.log(age)
+//nom = "tyufk"
+//age = 18;
+//console.log(age)
+
+
+
+
+
+const motDePasseAttendu = "123";
+let motDePasseUtilisateur = "123";
+
+
+if (motDePasseAttendu===motDePasseUtilisateur) {
+    console.log("Accès autorisé. Bienvenue !");
+} else if (motDePasseUtilisateur.length<8) {
+    console.log("Accès refusé. Votre mot de passe est trop court (8 caractères min).");
+} else {
+    console.log("Accès refusé. Mot de passe incorrect.");
+}
+
+
